9c6af7daec9c6b6eda75aee7553d2de66ba2660e
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-04-18 11:45:33 -0700

    Time: 36 ms (32.83%), Space: 13.8 MB (55.13%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -9,6 +9,7 @@ class Solution:
             res.append(word2[j])
             i += 1
             j += 1
+            
         res.append(word1[i:])
         res.append(word2[j:])
         return "".join(res)
