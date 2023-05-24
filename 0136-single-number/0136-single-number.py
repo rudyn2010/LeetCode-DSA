070588d4ea0786202de5acd31e6da4a7c3f81e6f
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-05-24 10:49:30 -0700

    Time: 147 ms (36.59%), Space: 19.4 MB (7.37%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        my_dictionary = {}
+        
+        for x in nums:
+            if x in my_dictionary:
+                my_dictionary[x] += 1
+            else:
+                my_dictionary[x] = 1
+                
+        singles = [sv for sv in my_dictionary if my_dictionary[sv] == 1]
+        return singles[0]
+    
