1be42b00731ab191f198e6d34204a654afca710e
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-06-05 22:10:29 -0700

    Time: 62 ms (7.47%), Space: 16.5 MB (47.32%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,6 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        #using a deque here for some micro-optimazation, we can append elements from the list to the begining of the deque, saving time from appending to the end and reversing
         string_constructor = collections.deque()
         
         start, i = -1, 0
@@ -19,4 +18,9 @@ class Solution:
             i += 1
         
         return " ".join(string_constructor)
-        
+    
+    #Using a Deque (Doubly Ended Queue) here for some micro-optimazation, 
+    #we can append elements from the list to the begining of the container, 
+    #saving time from having to append to the end and reversing.
+    #By appending to elements to the left or the front of the container,
+    #it'll give us the same result as reversing it in the end. 
