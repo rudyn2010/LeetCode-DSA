2d7dc96b5e798f25347ba713348940fa6d9e8856
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-06-08 16:40:02 -0700

    Time: 60 ms (49.07%), Space: 42.4 MB (24.21%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+    if (haystack === "") {
+        return -1 
+    }
+    
+    let start = 0;
+    let end = needle.length;
+    
+    while (start < haystack.length) {
+        let word = haystack.slice(start, end);
+        if (word === needle) {
+            return start;
+        };
+        start += 1
+        end += 1
+    }
+    return -1
+};
+
+/* First edge case is to check if haystack is an empty.
+Next loop through haystack length starting at 0 and use 
+.slice to verify word. 2nd arg passed into method is 
+non-inclusive*/
