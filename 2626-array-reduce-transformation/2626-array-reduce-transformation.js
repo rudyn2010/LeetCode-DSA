2032077548a975e65450617649acf66970e6c622
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-09-04 10:14:39 -0700

    Time: 63 ms (23.57%), Space: 42.2 MB (60.86%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let res = init;
+    
+    nums.forEach((n) => {
+        res = fn(res, n);
+    });
+    
+    return res;
+};
