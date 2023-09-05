54c91d357aa02182d058e5f73f96ff8569186d61
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-09-04 19:03:50 -0700

    Time: 43 ms (97.30%), Space: 43.6 MB (15.06%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,11 +5,19 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
+    
     let res = init;
     
+    /*
     nums.forEach((n) => {
         res = fn(res, n);
     });
+    
+    */
+    
+    for (let n of nums) {
+        res = fn(res, n)
+    };
     
     return res;
 };
