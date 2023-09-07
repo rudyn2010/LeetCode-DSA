667384144e6bff97426bcaa7bbc50fbf6773412f
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-09-06 22:55:25 -0700

    Time: 57 ms (32.41%), Space: 41.5 MB (83.19%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let count = 0;
+    
+    for (let i = 0; i < args.length; i++) {
+        count++;
+    };
+    return count;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
