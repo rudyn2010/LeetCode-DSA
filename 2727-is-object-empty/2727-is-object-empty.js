5204d6bcd6d24f06721708159c18e0961e5afc59
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-09-11 21:26:17 -0700

    Time: 45 ms (90.60%), Space: 42.7 MB (41.36%) - LeetHub

--- 2727-is-object-empty/2727-is-object-empty.js
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -5,8 +5,8 @@
 var isEmpty = function(obj) {
     
     if (typeof(obj) == "object") {
-        return Object.keys(obj).length == 0
-    }
+        return Object.keys(obj).length == 0;
+    };
     
-    return obj.length == 0
+    return obj.length == 0;
 };
